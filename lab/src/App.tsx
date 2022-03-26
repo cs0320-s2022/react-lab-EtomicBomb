265a778be8d187c88a22b113528790db0a804642
EtomicBomb <ethan@ethan.ws>
2022-03-26 12:46:12 -0400

    added files

---
+++ lab/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hororscope from './Horoscope';
+
+import logo from './logo.svg';
+import './App.css';
+import Horoscope from "./Horoscope";
+
+function App() {
+  return (
+    <div className="App">
+      <Horoscope />
+    </div>
+  );
+}
+
+export default App;
